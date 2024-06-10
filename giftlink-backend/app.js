ac4d4daf063c5e5d432138a1afda029fb1c092df
sunkarwar <sunkarwar@gmail.com>
2024-06-10 09:02:55 -0400

    connected giftRoutes in server

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 
 // Route files
 // Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-app.use("/gifts",giftRoutes)
+app.use("/api/gifts",giftRoutes)
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
 
